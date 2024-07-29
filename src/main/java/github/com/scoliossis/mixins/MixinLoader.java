f1ffa0cf5e05b42cc95d22dd0895b7cb54deb743
scale <152733080+scoliossis@users.noreply.github.com>
2024-07-29 18:51:32 +0200

    unfucked mixins

--- src/main/java/github/com/scoliossis/mixins/MixinLoader.java
+++ src/main/java/github/com/scoliossis/mixins/MixinLoader.java
@@ -1,9 +1,11 @@
 package github.com.scoliossis.mixins;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
+import javax.annotation.Nullable;
 import java.util.Map;
 
 public class MixinLoader implements IFMLLoadingPlugin {
@@ -12,35 +14,39 @@ public class MixinLoader implements IFMLLoadingPlugin {
     // loads the mixins!!
     public MixinLoader() {
         MixinBootstrap.init();
-        System.out.println("mixins up");
         Mixins.addConfiguration("mixins.scoliossis.json");
+        System.out.println("mixins up");
     }
 
 
 
     // class implements IFMLLoadingPlugin, must implement the methods, intellij does this for me
+    @NotNull
     @Override
     public String[] getASMTransformerClass() {
         return new String[0];
     }
 
+    @Nullable
     @Override
     public String getModContainerClass() {
-        return "";
+        return null;
     }
 
+    @Nullable
     @Override
     public String getSetupClass() {
-        return "";
+        return null;
     }
 
     @Override
-    public void injectData(Map<String, Object> map) {
+    public void injectData(Map<String, Object> data) {
 
     }
 
+    @Nullable
     @Override
     public String getAccessTransformerClass() {
-        return "";
+        return null;
     }
 }
